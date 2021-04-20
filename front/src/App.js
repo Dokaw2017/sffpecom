fe9423ea64317c7ef42071f7b3877471cfaacd98
Nebi <mam10122011@yahoo.com>
2021-04-20 11:18:56 +0300

    whatever

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,9 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <h2>Hello World!</h2>
     </div>
   );
---
+++ front/src/components/Header/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.scss";
+
+const Header = (props) => {
+  return <Header className="header"></Header>;
+};
+
+export default Header;
